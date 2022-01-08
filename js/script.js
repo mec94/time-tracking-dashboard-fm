b48ab6ff7430649ed3ee4704dce2a11837743a46
Mijail Cuesta <mec_94@outlook.com.ar>
2022-01-08 18:39:46 -0300

    Use of template string in function

--- js/script.js
+++ js/script.js
@@ -1,114 +1,122 @@
 const trackData = [
-    {
-      "title": "Work",
-      "timeframes": {
-        "daily": {
-          "current": 5,
-          "previous": 7
-        },
-        "weekly": {
-          "current": 32,
-          "previous": 36
-        },
-        "monthly": {
-          "current": 103,
-          "previous": 128
-        }
-      }
-    },
-    {
-      "title": "Play",
-      "timeframes": {
-        "daily": {
-          "current": 1,
-          "previous": 2
-        },
-        "weekly": {
-          "current": 10,
-          "previous": 8
-        },
-        "monthly": {
-          "current": 23,
-          "previous": 29
-        }
-      }
-    },
-    {
-      "title": "Study",
-      "timeframes": {
-        "daily": {
-          "current": 0,
-          "previous": 1
-        },
-        "weekly": {
-          "current": 4,
-          "previous": 7
-        },
-        "monthly": {
-          "current": 13,
-          "previous": 19
-        }
-      }
-    },
-    {
-      "title": "Exercise",
-      "timeframes": {
-        "daily": {
-          "current": 1,
-          "previous": 1
-        },
-        "weekly": {
-          "current": 4,
-          "previous": 5
-        },
-        "monthly": {
-          "current": 11,
-          "previous": 18
-        }
-      }
-    },
-    {
-      "title": "Social",
-      "timeframes": {
-        "daily": {
-          "current": 1,
-          "previous": 3
-        },
-        "weekly": {
-          "current": 5,
-          "previous": 10
-        },
-        "monthly": {
-          "current": 21,
-          "previous": 23
-        }
-      }
-    },
-    {
-      "title": "Self Care",
-      "timeframes": {
-        "daily": {
-          "current": 0,
-          "previous": 1
-        },
-        "weekly": {
-          "current": 2,
-          "previous": 2
-        },
-        "monthly": {
-          "current": 7,
-          "previous": 11
-        }
+  {
+    "title": "Work",
+    "timeframes": {
+      "daily": {
+        "current": 5,
+        "previous": 7
+      },
+      "weekly": {
+        "current": 32,
+        "previous": 36
+      },
+      "monthly": {
+        "current": 103,
+        "previous": 128
       }
     }
-  ]
+  },
+  {
+    "title": "Play",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 2
+      },
+      "weekly": {
+        "current": 10,
+        "previous": 8
+      },
+      "monthly": {
+        "current": 23,
+        "previous": 29
+      }
+    }
+  },
+  {
+    "title": "Study",
+    "timeframes": {
+      "daily": {
+        "current": 0,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 4,
+        "previous": 7
+      },
+      "monthly": {
+        "current": 13,
+        "previous": 19
+      }
+    }
+  },
+  {
+    "title": "Exercise",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 4,
+        "previous": 5
+      },
+      "monthly": {
+        "current": 11,
+        "previous": 18
+      }
+    }
+  },
+  {
+    "title": "Social",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 3,
+      },
+      "weekly": {
+        "current": 5,
+        "previous": 10,
+      },
+      "monthly": {
+        "current": 21,
+        "previous": 23,
+      }
+    }
+  },
+  {
+    "title": "Self Care",
+    "timeframes": {
+      "daily": {
+        "current": 0,
+        "previous": 1,
+      },
+      "weekly": {
+        "current": 2,
+        "previous": 2,
+      },
+      "monthly": {
+        "current": 7,
+        "previous": 11,
+      },
+    },
+  },
+];
 
-var frametimeHours = document.body.getElementsByClassName('report-frametime__hours');
-var frametimeLastWeek = document.body.getElementsByClassName('report-frametime__lastweek');
+const pickDate = {
+  daily: "Day",
+  weekly: "Week",
+  monthly: "Month"
+}
+
+const frametimeHours =
+  document.getElementsByClassName('report-frametime__hours');
+const frametimePrevious =
+  document.getElementsByClassName('report-frametime__lastweek');
 
 function switchDate(choice) {
-    for(let i=0; i < frametimeHours.length; i++) {
+  for (let i = 0; i < frametimeHours.length; i++) {
     frametimeHours[i].innerHTML = trackData[i].timeframes[choice].current + "hrs";
-    frametimeLastWeek[i].innerText = "Last Week - " + trackData[i].timeframes[choice].previous + "hrs";
+    frametimePrevious[i].innerText = `Last ${pickDate[choice]} - ${trackData[i].timeframes[choice].previous} hrs`;
   }
 }
